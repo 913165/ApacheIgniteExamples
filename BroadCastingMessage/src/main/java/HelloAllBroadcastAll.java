929eb2f7d31dfa20d58c5940dd3310910340653f
913165 <tinumistry@gmail.com>
2020-03-01 20:38:12 +0530

    Add files via upload

--- BroadCastingMessage/src/main/java/HelloAllBroadcastAll.java
+++ BroadCastingMessage/src/main/java/HelloAllBroadcastAll.java
@@ -1,14 +1,22 @@
 import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCluster;
 import org.apache.ignite.IgniteCompute;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cluster.ClusterGroup;
 import org.apache.ignite.configuration.IgniteConfiguration;
 
 public class HelloAllBroadcastAll {
 	public static void main(String[] args) {
 		IgniteConfiguration cfg = new IgniteConfiguration();
 		cfg.setPeerClassLoadingEnabled(true);
-		cfg.setClientMode(true);
+		// cfg.setClientMode(true);
+
 		try (Ignite ignite = Ignition.start(cfg)) {
+			IgniteCluster cluster = ignite.cluster();
+			// Get remote server group
+			//ClusterGroup forRemotes = cluster.forRemotes();
+			
+			
 			IgniteCompute compute = ignite.compute();
 			// broadcast the computation to all nodes
 			compute.broadcast(() -> {
